d9392cce911764cb0584220cdaa5aad7a58f0375
LeandroDreary <l3androviegas@gmail.com>
2021-11-01 23:56:56 -0300

    Using module.exports instead of export default

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
 
 const baseURL = "https://www.frasesdobem.com.br/page"
 
-export default async ({ s = "", page = 1 }) => {
+module.exports = async ({ s = "", page = 1 }) => {
     let quotes = []
 
     await fetch(`${baseURL+"/"+page}?` + new URLSearchParams({
